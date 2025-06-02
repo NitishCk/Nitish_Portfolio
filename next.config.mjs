5c93c1853dc024e8473bbc144cb7445c4def0741
Nitish CK <cknitish02@gmail.com>
2025-06-02 11:34:32 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Nitish_Portfolio',
+  assetPrefix: '/Nitish_Portfolio',
 };
 
 export default nextConfig;
