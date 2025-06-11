099e5a64a1baab6fbd16bb1d28b994cd8ec10a7f
Nitish CK <cknitish02@gmail.com>
2025-06-11 11:47:48 +0530

    first commit

--- utils/mySelf.js
+++ utils/mySelf.js
@@ -6,9 +6,9 @@ const mySelf = {
     `
         I have always been passionate about technology and building innovative solutions for the web. My journey into programming began with curiosity—exploring how things work and pushing boundaries to create something impactful.`,
     `
-        With a strong foundation in Java, C++, and JavaScript, I have developed projects ranging from full-stack applications to custom reusable components that enhance efficiency. During my time at Newgen Software, I contributed to building scalable solutions using React, Java, and SQL, improving product functionality and user experience.`,
+        With a strong foundation in Java, C++, and JavaScript, I have developed projects ranging from full-stack applications to custom reusable components that enhance efficiency. During my time at Kanshe Infotech, I contributed to building scalable solutions using React, Java, and SQL, improving product functionality and user experience.`,
     `
-        Beyond coding, I love tackling complex challenges, whether it’s optimizing a system or designing seamless user interactions. My work on SocialBuddies and Hotstar-Clone showcases my ability to integrate real-time features, authentication systems, and dynamic interfaces.`,
+        Beyond coding, I love tackling complex challenges, whether it’s optimizing a system or designing seamless user interactions. My work on Food, Fresh Cart, Odyssey Travels showcases my ability to integrate real-time features, authentication systems, and dynamic interfaces.`,
     `
         For me, programming is more than just writing code—it's about crafting meaningful experiences and solving real-world problems. With every project, I strive to refine my skills, collaborate with diverse teams, and bring ideas to life in the most efficient way possible.
         `,
